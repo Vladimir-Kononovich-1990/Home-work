dbb6dcc630ea98fac6f1c6d3a62c0b7a72e9ffdf
Vladimir-Kononovich-1990 <94787565+Vladimir-Kononovich-1990@users.noreply.github.com>
2021-12-11 00:24:52 +0300

    Task5

---
+++ home_work_1/Task5.java
@@ -0,0 +1,29 @@
+package Home_work_1;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		System.out.print(">>");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		//System.out.println(s);
+		String s1 = "Вася";
+		String s2 = "Анастасия";
+		
+		if (Objects.equals(s,s1)) {
+			System.out.println("Привет!");
+		    System.out.println("Я тебя так долго ждал");
+		    }
+		
+		if (Objects.equals(s,s2)) {
+			System.out.println("Я тебя так долго ждал");	
+		}
+		
+		if (Objects.equals(s,s1)==false && Objects.equals(s,s2)==false)
+			System.out.println("Добрый день, а вы кто?");
+
+	}
+
+}
